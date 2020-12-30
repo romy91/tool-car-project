44ecf3ff02a0e50fe7497e47e53b71374e825e76
Romilio Corona <romilioc1@verifone.com>
2020-12-29 23:44:30 -0300

    feature/tc-add-angular-material - Added angular material and initial modules

---
+++ src/app/modules/patent-rolls/patent-rolls.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PatentRollsRoutingModule } from './patent-rolls-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PatentRollsRoutingModule
+  ]
+})
+export class PatentRollsModule { }
